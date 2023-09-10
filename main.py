54a53df6ad7469195141ac4375b8f4e66692f55d
Afiq Nazir <62737721+afiqnazir@users.noreply.github.com>
2023-09-10 17:15:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # copyright 2020-22 @Mohamed Rizad
-# Telegram @riz4d
+# Telegram @afiqnazir
 # Instagram @riz.4d
 from pyrogram import *
 import requests as re
@@ -34,7 +34,7 @@ app=Client('Temp-Mail Bot',
 email=''
 @app.on_message(filters.command('start'))
 async def start_msg(client,message):
-    await message.reply("**Hey "+message.from_user.first_name+" !!**\n @mysterymailbot is a free service that allows to generates and receive emails at a temporary address that self-destructed after a certain time elapses.\n\n**__ How It Safe's You??**__\n- Using the temporary mail allows you to completely protect your real mailbox against the loss of personal information. Your temporary e-mail address is completely anonymous. Your details: information about your person and users with whom you communicate, IP-address, e-mail address are protected and completely confidential.\n\nFurther Queris @riz4d🌚")
+    await message.reply("**Hey "+message.from_user.first_name+" !!**\n @mrxmailbot is a free service that allows to generates and receive emails at a temporary address that self-destructed after a certain time elapses.\n\n**__ How It Safe's You??**__\n- Using the temporary mail allows you to completely protect your real mailbox against the loss of personal information. Your temporary e-mail address is completely anonymous. Your details: information about your person and users with whom you communicate, IP-address, e-mail address are protected and completely confidential.\n\nFurther Queris @afiqnazir🌚")
     await message.reply("**Generate a Email Now❕**",
                         reply_markup=buttons)
 @app.on_callback_query()
